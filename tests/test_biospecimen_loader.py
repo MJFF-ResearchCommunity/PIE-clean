6ad6818425773040a5e1651b81f78d94f835f453
Victoria Catterson <victoria@biosymetrics.com>
2025-11-04 14:06:11 -0800

    Tests for load_metabolomic_lrrk2

--- tests/test_biospecimen_loader.py
+++ tests/test_biospecimen_loader.py
@@ -16,7 +16,6 @@ SUB_DIR = "Biospecimen"
 def test_load_project_151(caplog, tmp_path):
     # Defaults to batch_corrected=False
     df = load_project_151_pQTL_CSF(f"{DATA_DIR}/{SUB_DIR}")
-
     # Logging shows that the files were found correctly
     assert "Successfully processed Project_151" in caplog.records[-1].message
     # Logging shows the data was pivoted into wide format
@@ -54,6 +53,62 @@ def test_load_project_151(caplog, tmp_path):
         tmp.to_csv(file, index=False)
 
     df = load_project_151_pQTL_CSF(tmp_path)
+    assert caplog.records[-1].levelname == "ERROR"
+    assert "Required column PATNO not found" in caplog.records[-1].message
+    assert df.empty
+
+def test_load_metabolomic_lrrk2(caplog, tmp_path):
+    # First, skip the CSF files
+    df = load_metabolomic_lrrk2(f"{DATA_DIR}/{SUB_DIR}", include_csf=False)
+    # Logging shows that the files were found correctly
+    assert "Successfully processed Metabolomic" in caplog.records[-1].message
+    # Logging shows the data was pivoted into wide format
+    assert "2 rows, 8 columns" in caplog.records[-1].message
+    assert df.shape[0] == 2 # ...and the output matches the logging
+    # PATNOs from each file have been merged together (ints here)
+    assert 9999 in df["PATNO"].tolist()
+    assert 9998 in df["PATNO"].tolist()
+    # Test IDs have become column names, with "LRRK2_" prepended
+    assert "LRRK2_MZ100.08_RT586.24_pos" in df.columns.tolist()
+    # But the CSF tests are not included
+    assert "LRRK2_(3-O-sulfo)GalCer(d18:1/16:0)" not in df.columns.tolist()
+    # Others have been dropped
+    assert "UNITS" not in df.columns.tolist()
+    # But SEX was kept
+    assert "SEX" in df.columns.tolist()
+    # CLINICAL_EVENT has been renamed to EVENT_ID
+    assert "CLINICAL_EVENT" not in df.columns.tolist()
+    assert "EVENT_ID" in df.columns.tolist()
+    assert (df["EVENT_ID"] == "V04").all(), f"Non-V04 EVENT_IDs included: {df['EVENT_ID'].unique()}"
+    # And data is complete
+    assert not df.isnull().any().any()
+
+    # Next, test the CSF switch defaults to True
+    df = load_metabolomic_lrrk2(f"{DATA_DIR}/{SUB_DIR}")
+    # Logging shows that the files were found correctly
+    assert "Successfully processed Metabolomic" in caplog.records[-1].message
+    # Logging shows more rows and more columns
+    assert "4 rows, 9 columns" in caplog.records[-1].message
+    assert df.shape[0] == 4 # ...and the output matches the logging
+    # Now the CSF tests are included
+    assert "LRRK2_(3-O-sulfo)GalCer(d18:1/16:0)" in df.columns.tolist()
+    # And we have V06 events as well as the original V04s
+    assert "V04" in df["EVENT_ID"].tolist()
+    assert "V06" in df["EVENT_ID"].tolist()
+    # V06 non-CSF tests are null
+    assert df[df["EVENT_ID"]=="V04"]["LRRK2_MZ100.08_RT586.24_pos"].notnull().all()
+    assert df[df["EVENT_ID"]=="V06"]["LRRK2_MZ100.08_RT586.24_pos"].isnull().all()
+
+    # Test when required column is missing: set up missing PATNO files
+    testfile = "Metabolomic_Analysis_of_LRRK2_PD"
+    shutil.copytree(f"{DATA_DIR}/{SUB_DIR}", tmp_path / SUB_DIR, dirs_exist_ok=True)
+    for n in [1, 2]:
+        file = tmp_path / SUB_DIR / f"{testfile}_{n}_of_5_21Test2025.csv"
+        tmp = pd.read_csv(file)
+        tmp = tmp.drop(columns="PATNO")
+        tmp.to_csv(file, index=False)
+
+    df = load_metabolomic_lrrk2(tmp_path, include_csf=False)
     assert caplog.records[-1].levelname == "ERROR"
     assert "Required column PATNO not found" in caplog.records[-1].message
     assert df.empty
